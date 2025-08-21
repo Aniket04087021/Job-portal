d0a363a6aa349657cc8a1418cf019544ea4081b3
ANIKET GUPTA <139427075+Aniket04087021@users.noreply.github.com>
2025-08-21 23:56:06 +0530

    Update Jobs.jsx

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import FilterCard from './FilterCard'
 
-import Job from './job'
+import Job from './Job'
 
 const jobsArray = [1,2,3,4,5,6,7,8,9]
 
